af89ca9e6229f611971764146575774106e8919b
vvbereziuk <bereziuk.vg@gmail.com>
2023-09-02 14:39:18 +0300

    hw6

--- lesson6/script6.js
+++ lesson6/script6.js
@@ -1,38 +1,131 @@
 // - Знайти та вивести довижину настипних стрінгових значень
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+let str1 = 'hello world';
+let str2 = 'lorem ipsum';
+let str3 = 'javascript is cool';
+console.log(str1.length, str2.length, str3.length)
+
 // - Перевести до великого регістру наступні стрінгові значення
 // 'hello world', 'lorem ipsum', 'javascript is cool'
+console.log(str1.toUpperCase(), str2.toUpperCase(), str3.toUpperCase())
+
 // - Перевести до нижнього регістру настипні стрінгові значення
 // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+let str4 = 'HELLO WORLD';
+let str5 = 'LOREM IPSUM';
+let str6 = 'JAVASCRIPT IS COOL';
+console.log(str4.toLowerCase(), str5.toLowerCase(), str6.toLowerCase())
+
 // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
-//
-// - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+let str7 = ' dirty string   '
+console.log(str7.trim());
+
+
+// - На пишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
 //     let str = 'Ревуть воли як ясла повні';
-// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']
-//
+// let arr = stringToarray(str); ['Ревуть', 'воли', 'як', 'ясла', 'повні']//
+let str = 'Ревуть воли як ясла повні';
+let stringToArray = str.split(' ')
+console.log(stringToArray);
+
+
 // - є масив чисел [10,8,-7,55,987,-1011,0,1050,0] . за допомоги map  перетворити всі об'єкти в масиві на стрінгові.
-// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки в залежності від значення аргументу direction.
-//     let nums = [11,21,3];
+let arrNumb = [10, 8, -7, 55, 987, -1011, 0, 1050, 0];
+let arrToString = arrNumb.map(value => value.toString())
+// або
+// let arrToString = arrNumb.map(value => String(value));
+// або
+// let arrToString = arrNumb.map(arrNumb => '' + arrNumb);
+console.log(arrToString);
+
+// - створити функцію sortNums(direction), яка прймає масив чисел, та сортує його від більшого до меньшого, або навпаки
+// в залежності від значення аргументу direction.
+// let nums = [11, 21, 3];
 // sortNums(nums,'ascending') // [3,11,21]
 // sortNums(nums,'descending') // [21,11,3]
-//
+let nums = [11, 21, 3];
+let sortNums = (nums, direction) => {
+    if (direction === 'ascending') {
+        nums.sort((a, b) => a - b)
+    } else if (direction === 'descending') {
+        nums.sort((a, b) => b - a)
+    }
+    return nums;
+}
+console.log(sortNums(nums, 'ascending'));
+console.log(sortNums(nums, 'descending'));
+
+
 // ==========================
 // - є масив
-// let coursesAndDurationArray = [
-//     {title: 'JavaScript Complex', monthDuration: 5},
-//     {title: 'Java Complex', monthDuration: 6},
-//     {title: 'Python Complex', monthDuration: 6},
-//     {title: 'QA Complex', monthDuration: 4},
-//     {title: 'FullStack', monthDuration: 7},
-//     {title: 'Frontend', monthDuration: 4}
-// ];
+let coursesAndDurationArray = [
+    {title: 'JavaScript Complex', monthDuration: 5},
+    {title: 'Java Complex', monthDuration: 6},
+    {title: 'Python Complex', monthDuration: 6},
+    {title: 'QA Complex', monthDuration: 4},
+    {title: 'FullStack', monthDuration: 7},
+    {title: 'Frontend', monthDuration: 4}
+];
 // -- відсортувати його за спаданням за monthDuration
+let mouthDurationDecline = coursesAndDurationArray.sort((a, b) => {
+    return b.monthDuration - a.monthDuration
+})
+console.log(mouthDurationDecline)
+
+// АБО
+// console.log(coursesAndDurationArray.sort((a, b) => {
+//     if( a.monthDuration < b.monthDuration) {
+//         return 1;
+//     }
+//     if(a.monthDuration > b.monthDuration){
+//         return -1;
+//     }
+//     if(a.monthDuration === b.monthDuration)
+//         return 0
+// }))
+
+
 // -- відфільтрувати , залишивши тільки курси з тривалістю більше 5 місяців
+let mouthDurationFilter = coursesAndDurationArray.filter(value => value.monthDuration > 5)
+console.log(mouthDurationFilter)
+
 // -- за допомоги map перетворити кожен елемент на наступний тип {id,title,monthDuration}
+let mouthDurationId = coursesAndDurationArray.map((value, index) => {
+    return {id: index + 1, ...value}
+})
+console.log(mouthDurationId)
+
 // =========================
 //     описати колоду карт (від 6 до туза без джокерів)
+let cards = [
+    {value: '6', cardSuit: 'diamonds', color: 'red'},
+    {value: '6', cardSuit: 'hearts', color: 'red'},
+    {value: '7', cardSuit: 'clubs', color: 'black'},
+    {value: '7', cardSuit: 'spades', color: 'black'},
+    {value: '8', cardSuit: 'hearts', color: 'red'},
+    {value: '8', cardSuit: 'clubs', color: 'black'},
+    {value: '9', cardSuit: 'clubs', color: 'black'},
+    {value: '9', cardSuit: 'diamonds', color: 'red'},
+    {value: '10', cardSuit: 'diamonds', color: 'red'},
+    {value: '10', cardSuit: 'spades', color: 'black'},
+    {value: 'J', cardSuit: 'diamonds', color: 'red'},
+    {value: 'J', cardSuit: 'spades', color: 'black'},
+    {value: 'Q', cardSuit: 'hearts', color: 'red'},
+    {value: 'Q', cardSuit: 'clubs', color: 'black'},
+    {value: 'K', cardSuit: 'clubs', color: 'black'},
+    {value: 'K', cardSuit: 'diamonds', color: 'red'},
+    {value: 'A', cardSuit: 'hearts', color: 'red'},
+    {value: 'A', cardSuit: 'spades', color: 'red'}
+
+]
 // - знайти піковий туз
+let diamondsAce = cards.find(val => val.cardSuit === 'spades' && val.value === 'A')
+console.log(diamondsAce)
+
 // - всі шістки
+let sixTh = cards.filter(val => val.value === '6')
+console.log(sixTh);
+
 // - всі червоні карти
 // - всі буби
 // - всі трефи від 9 та більше
