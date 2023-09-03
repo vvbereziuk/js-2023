c47f284b88c937b3aa72e9ed511c83954515ac7b
vvbereziuk <bereziuk.vg@gmail.com>
2023-09-03 22:15:39 +0300

    hw6

---
+++ lesson6/add/scriptAdd.js
@@ -0,0 +1,271 @@
+// - Напишите функцию cutString(str, n), которая делит строку на подстроки, состоящие из n символов.
+// document.writeln(cutString('наслаждение',3)) // [нас,лаж,ден,ие]
+function cutString(str, n) {
+    let arr = [];
+    for (let i = 0; i < str.length; i += n)
+        arr.push(str.slice(i, i + n))
+    return arr
+}
+
+document.write(cutString('наслаждение', 3));
+
+// - Напишіть функцію delete_characters(str, length), яка повертає підрядок, що складається із зазначеної кількості символів.
+//     let str = 'Каждый охотник желает знать';
+// document.writeln(delete_characters(str, 7)); // Каждый
+let str = 'Каждый охотник желает знать';
+
+function delete_characters(str, length) {
+    return str.slice(0, length)
+}
+
+document.write(delete_characters(str, 7));
+
+
+// - Напишіть функцію insert_dash(str), яка приймає рядок str як аргумент і вставляє тире (-) між словами. При цьому всі символи рядка необхідно перевести у верхній регістр.
+//     let str = "HTML JavaScript PHP";
+// document.writeln(insert_dash(str)); // 'HTML-JAVASCRIPT-PHP'
+let str1 = "HTML JavaScript PHP";
+
+function insert_dash(str) {
+    return str.toUpperCase().split(' ').join('-')
+}
+
+document.write(insert_dash(str1))
+
+// - Напишіть функцію, яка приймає рядок як аргумент і перетворює регістр першого символу рядка з нижнього регістру у верхній.
+//
+// - Дано список імен.
+//     let n1 = 'Harry..Potter'
+// let n2 = 'Ron---Whisley'
+// let n3 = 'Hermione__Granger'
+
+// Написати функцію, яка приймає будь яке не валідне імя, та нормалізує його в наступнйи вигляд
+let n1 = 'Harry Potter'
+
+function normalize(str) {
+    let arr = str.split(' ')
+    let arr1 = []
+    for (let i = 0; i < arr.length; i++) {
+        arr1.push(arr[i][0].toUpperCase() + arr[i].slice(1))
+    }
+    return arr1.join(' ')
+}
+
+document.write(normalize('jeki chan'))
+
+//     - створити функцію, яка генерує масив рандомних числових цілих значень в діапазоні від 0 до 100.
+function randomNums() {
+    let arr = []
+    for (let i = 0; i < 15; i++) {
+        arr.push(Math.floor(Math.random() * 100))
+    }
+    return arr
+}
+
+console.log(randomNums())
+
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. Відсортувати його за допомоги sort
+let randomSort = randomNums().sort((a, b) => a - b)
+console.log(randomSort)
+
+// - створити (або згенерувати, за допомоги попередньої функції) масив рандомних цілих числових значень. відфільтрувати  його за допомоги filter,
+// залишивши тільки парні числа (без 0!)
+let randomFilter = randomNums().filter((a) => a % 2 === 0)
+console.log(randomFilter)
+
+// - Напишіть функцію capitalize(str), яка повертає рядок, у якому кожне слово починається з великої літери.
+function capitalize(str) {
+    let arr = str.split(' ')
+    let arr1 = []
+    for (let i = 0; i < arr.length; i++) {
+        arr1.push(arr[i][0].toUpperCase() + arr[i].slice(1))
+    }
+    return arr1.join(' ')
+}
+
+console.log(capitalize('good morning, how are you?'))
+
+// - Створити функцію-валідатор для адрес електронної пошти. Перевірка повинна включати в себе :данні до знака равлика(@), наявність равлика, крапку яка знаходиться не меньше ніж на 2 символ далі після равлика, функція не чутлива до регістру (some@email.com,SOME@EMAIL.COM,some@EMAIL.com, і тд - однакові значення)
+// Протестувати на значеннях
+// someemail@gmail.com
+// someeMAIL@gmail.com
+// someeMAIL@i.ua
+// some.email@gmail.com
+//
+// Примітка
+// Для тих, хто дуже розумний, та почне використовувати регулярні вирази одразу "ні". Своїм мозком подумайте над протоколом, з регулярками будете потім бавитись.
+//
+// - є масив
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+// відсортувати його в спадаючому порядку за кількістю елементів в полі modules
+console.log(coursesArray.sort((a, b) => {
+    return b.modules.length - a.modules.length
+}))
+
+// - Напишіть функцію count(str, stringsearch), яка повертає кількість символів stringsearch у рядку str.
+//     let symb = "о", str = "Астрономия это наука о небесных объектах";
+// document.writeln(count(str, symb)) // 5
+let symb = 'о'
+let str2 = "Астрономия это наука о небесных объектах"
+
+function count(str, stringsearch) {
+    for (const strElement of str) {
+        if (strElement === stringsearch) {
+            return str.split(stringsearch).length - 1
+        }
+    }
+}
+
+document.write(count(str2, symb))
+
+
+// - Напишіть функцію cutString(str, n), яка видаляє зайві слова з рядка str, залишивши у ній n слів.
+//     let str = "Сила тяжести приложена к центру масс тела";
+// document.writeln(cutString(str, 5)) // 'Сила тяжести приложена к центру'
+let str3 = "Сила тяжести приложена к центру масс тела";
+
+function cutStringWord(str, n) {
+    return str.split(' ').slice(0, n).join(' ')
+}
+
+document.write(cutStringWord(str3, 5))
+
+// -стоврити масив книжок (назва, кількість сторінок, автори , жанри).
+
+let books = [
+    {
+        title: "Пригоди Тома Сойєра",
+        pageCount: 224,
+        authors: ["Марк Твен"],
+        genres: ["Пригоди", "Класика"]
+    },
+    {
+        title: "Гра Престолів",
+        pageCount: 694,
+        authors: ["Джордж Мартін"],
+        genres: ["Фентезі"]
+    },
+    {
+        title: "Чорне Лебедя: Вплив непередбачуваних подій",
+        pageCount: 464,
+        authors: ["Нассім Талеб"],
+        genres: ["Наука", "Філософія"]
+    },
+    {
+        title: "Війна і мир",
+        pageCount: 1225,
+        authors: ["Лев Толстой"],
+        genres: ["Роман", "Історичний"]
+    },
+    {
+        title: "Шерлок Холмс: Пригоди",
+        pageCount: 480,
+        authors: ["Артур Конан Дойл"],
+        genres: ["Детектив", "Містика", "Пригоди", "Класика"]
+    },
+    {
+        title: "Гордість і упередження",
+        pageCount: 432,
+        authors: ["Джейн Остін"],
+        genres: ["Роман", "Комедія"]
+    },
+    {
+        title: "Місто скал",
+        pageCount: 368,
+        authors: ["Касандра Клер"],
+        genres: ["Фентезі", "Пригоди"]
+    },
+    {
+        title: "1984",
+        pageCount: 328,
+        authors: ["Джордж Оруелл"],
+        genres: ["Наукова фантастика", "Антиутопія"]
+    }
+];
+
+// -знайти найбльша кількість сторінок.
+let maxPageCount = function() {
+    let max = 0;
+    for (const value of books) {
+        if (value.pageCount > max) {
+            max = value.pageCount
+        }
+    }
+    return max
+}
+console.log(maxPageCount())
+
+// - знайти книжку/ки з найбільшою кількістю жанрів
+// - знайти книжку/ки з найдовшою назвою
+// - знайти книжку/ки які писали 2 автори
+// - знайти книжку/ки які писав 1 автор
+// - вісортувати книжки по кількості сторінок по зростанню
--- lesson6/script6.js
+++ lesson6/script6.js
@@ -97,45 +97,73 @@ console.log(mouthDurationId)
 
 // =========================
 //     описати колоду карт (від 6 до туза без джокерів)
-let cards = [
-    {value: '6', cardSuit: 'diamonds', color: 'red'},
-    {value: '6', cardSuit: 'hearts', color: 'red'},
-    {value: '7', cardSuit: 'clubs', color: 'black'},
-    {value: '7', cardSuit: 'spades', color: 'black'},
-    {value: '8', cardSuit: 'hearts', color: 'red'},
-    {value: '8', cardSuit: 'clubs', color: 'black'},
-    {value: '9', cardSuit: 'clubs', color: 'black'},
-    {value: '9', cardSuit: 'diamonds', color: 'red'},
-    {value: '10', cardSuit: 'diamonds', color: 'red'},
-    {value: '10', cardSuit: 'spades', color: 'black'},
-    {value: 'J', cardSuit: 'diamonds', color: 'red'},
-    {value: 'J', cardSuit: 'spades', color: 'black'},
-    {value: 'Q', cardSuit: 'hearts', color: 'red'},
-    {value: 'Q', cardSuit: 'clubs', color: 'black'},
-    {value: 'K', cardSuit: 'clubs', color: 'black'},
-    {value: 'K', cardSuit: 'diamonds', color: 'red'},
-    {value: 'A', cardSuit: 'hearts', color: 'red'},
-    {value: 'A', cardSuit: 'spades', color: 'red'}
-
-]
-// - знайти піковий туз
-let diamondsAce = cards.find(val => val.cardSuit === 'spades' && val.value === 'A')
-console.log(diamondsAce)
-
-// - всі шістки
-let sixTh = cards.filter(val => val.value === '6')
-console.log(sixTh);
-
-// - всі червоні карти
-// - всі буби
-// - всі трефи від 9 та більше
-//
 // {
 //     cardSuit: '', // 'spade', 'diamond','heart', 'clubs'
 //         value: '', // '6'-'10', 'ace','jack','queen','king','joker'
 //     color:'', // 'red','black'
 // }
-//
+
+let cards = [
+    {value: '6', cardSuit: 'diamonds', color: 'red'},
+    {value: '6', cardSuit: 'hearts', color: 'red'},
+    {value: '6', cardSuit: 'clubs', color: 'black'},
+    {value: '6', cardSuit: 'spades', color: 'black'},
+    {value: '7', cardSuit: 'clubs', color: 'black'},
+    {value: '7', cardSuit: 'spades', color: 'black'},
+    {value: '7', cardSuit: 'diamonds', color: 'red'},
+    {value: '7', cardSuit: 'hearts', color: 'red'},
+    {value: '8', cardSuit: 'hearts', color: 'red'},
+    {value: '8', cardSuit: 'clubs', color: 'black'},
+    {value: '8', cardSuit: 'spades', color: 'black'},
+    {value: '8', cardSuit: 'diamonds', color: 'red'},
+    {value: '9', cardSuit: 'clubs', color: 'black'},
+    {value: '9', cardSuit: 'diamonds', color: 'red'},
+    {value: '9', cardSuit: 'hearts', color: 'red'},
+    {value: '9', cardSuit: 'spades', color: 'black'},
+    {value: '10', cardSuit: 'diamonds', color: 'red'},
+    {value: '10', cardSuit: 'spades', color: 'black'},
+    {value: '10', cardSuit: 'clubs', color: 'black'},
+    {value: '10', cardSuit: 'hearts', color: 'red'},
+    {value: 'J', cardSuit: 'diamonds', color: 'red'},
+    {value: 'J', cardSuit: 'hearts', color: 'red'},
+    {value: 'J', cardSuit: 'spades', color: 'black'},
+    {value: 'J', cardSuit: 'clubs', color: 'black'},
+    {value: 'Q', cardSuit: 'hearts', color: 'red'},
+    {value: 'Q', cardSuit: 'clubs', color: 'black'},
+    {value: 'Q', cardSuit: 'spades', color: 'black'},
+    {value: 'Q', cardSuit: 'diamonds', color: 'red'},
+    {value: 'K', cardSuit: 'clubs', color: 'black'},
+    {value: 'K', cardSuit: 'diamonds', color: 'red'},
+    {value: 'K', cardSuit: 'spades', color: 'black'},
+    {value: 'K', cardSuit: 'hearts', color: 'red'},
+    {value: 'A', cardSuit: 'hearts', color: 'red'},
+    {value: 'A', cardSuit: 'clubs', color: 'black'},
+    {value: 'A', cardSuit: 'diamonds', color: 'red'},
+    {value: 'A', cardSuit: 'spades', color: 'black'}
+
+
+]
+// - знайти піковий туз
+let diamondsAce = cards.find(val => val.cardSuit === 'spades' && val.value === 'A');
+console.log(diamondsAce)
+
+// - всі шістки
+let sixTh = cards.filter(val => val.value === '6');
+console.log(sixTh);
+
+// - всі червоні карти
+let redCards = cards.filter(val => val.color === 'red');
+console.log(redCards);
+
+// - всі буби
+let diamondsCards = cards.filter(val => val.cardSuit === 'diamonds')
+console.log(diamondsCards);
+
+// - всі трефи від 9 та більше
+let clubsCards = cards.filter(val => val.cardSuit === 'clubs' && val.value >= '9')
+console.log(clubsCards);
+
+
 // =========================
 //
 //     Взяти описану колоду карт, та за допомоги reduce упакувати всі карти по "мастях" в об'єкт
@@ -145,7 +173,101 @@ console.log(sixTh);
 //     hearts:[],
 //     clubs:[]
 // }
+let cardSuit = cards.reduce((accum, currentValue) => {
+    if (currentValue.cardSuit === 'spades') {
+        accum.spades.push(currentValue)
+    }
+    if (currentValue.cardSuit === 'diamonds') {
+        accum.diamonds.push(currentValue)
+    }
+    if (currentValue.cardSuit === 'hearts') {
+        accum.hearts.push(currentValue)
+    }
+    if (currentValue.cardSuit === 'clubs') {
+        accum.clubs.push(currentValue)
+    }
+    return accum
+}, {spades: [], diamonds: [], hearts: [], clubs: []})
+console.log(cardSuit);
+
+
 // =========================
 //     взяти з arrays.js (який лежить в папці 2023 plan) масив coursesArray
+let coursesArray = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'java core',
+            'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'angular',
+            'aws',
+            'docker',
+            'python core',
+            'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html',
+            'css',
+            'js',
+            'mysql',
+            'mongodb',
+            'react',
+            'angular',
+            'aws',
+            'docker',
+            'git',
+            'node.js',
+            'python',
+            'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
 // --написати пошук всіх об'єктів, в який в modules є sass
+let sassModules = coursesArray.filter(value => value.modules.includes('sass'))
+console.log(sassModules);
+
 // --написати пошук всіх об'єктів, в який в modules є docker
+let dockerModules = coursesArray.filter(value => value.modules.includes('docker'))
+console.log(dockerModules);
+
